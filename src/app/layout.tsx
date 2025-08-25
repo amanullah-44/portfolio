530d4d6ad50bdb8936d400cb7196eb6e59de9baf
amanu4519 <amanu4519@gmail.com>
2025-08-25 13:51:19 +0000

    added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,13 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
 const fontSans = FontSans({
+
+
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-const title=`${DATA.name} - Full Stack Developer`
+const title = `${DATA.name} - Full Stack Developer`
 
 export const metadata: Metadata = {
 
@@ -30,7 +32,7 @@ export const metadata: Metadata = {
     locale: "en_US",
     type: "website",
     images: DATA.avatarUrl,
-    
+
   },
   robots: {
     index: true,
@@ -48,20 +50,28 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   verification: {
- google: "MaSBJGB-PAPtXY5-Sy5wE-Nno6tUcOpYNWC40OAbl3Y",
+    google: "MaSBJGB-PAPtXY5-Sy5wE-Nno6tUcOpYNWC40OAbl3Y",
     yandex: "",
-
-    
   },
 };
+
+const GTM_CONTAINER_ID = "GTM-KBNNCQQW";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
+
+
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');` }} />
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
@@ -74,6 +84,9 @@ export default function RootLayout({
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   );
